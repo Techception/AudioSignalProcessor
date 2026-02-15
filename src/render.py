f545b8df128aeefe7016d6ce4564c342fb413205
techception <alexhall1993@hotmail.co.uk>
2026-02-15 17:37:52 +0000

    takes command line argumens for frquency

--- src/render.py
+++ src/render.py
@@ -6,7 +6,7 @@ Goal:
 
 import numpy as np
 import wave
-
+import sys
 
 def write_wav_mono(path: str, x: np.ndarray, sr: int) -> None:
     """write mono 16-bit PCM WAV. x is float32 in [-1,1]."""
@@ -23,11 +23,14 @@ def write_wav_mono(path: str, x: np.ndarray, sr: int) -> None:
 def main():
     sr = 48000 #sample rate
     dur_s = 10.0 #duration of play
-    f_hz = 300.0 #sound frequency
-
+    #f_hz = 300.0 #sound frequency
+    f_hz = float(sys.argv[1]) #input sound frequency
+    print(f_hz)
+    
     n = int(sr * dur_s) #total number of samples required
     t = np.arange(n, dtype=np.float32) / sr #time index
     x = 1 * np.sin(2.0 * np.pi * f_hz *t) 
+    print(x)
 
     write_wav_mono("out.wav", x, sr) 
     print("Wrote out.wav")
